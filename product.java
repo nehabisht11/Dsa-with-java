d773a8815be26c0a0c640ba14972c54f8fa17294
nehabisht11 <nehabisht1911@gmail.com>
2024-06-14 20:36:20 +0530

    codee

---
+++ product.java
@@ -0,0 +1,17 @@
+public class product {
+    public static int multiply(int a,int b){
+        int product =a*b;
+        return product;
+    }
+        public static void main(String[]args){
+            int product =multiply(2,4);
+            System.out.println(product);
+            product=multiply(12,23);
+            System.out.println(product);
+
+
+        }
+
+    }
+    
+
