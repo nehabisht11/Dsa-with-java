118fc553ebbbbf884668b32312694e770eff9eb9
nehabisht11 <nehabisht1911@gmail.com>
2024-07-03 11:06:50 +0530

    codee

--- inbltsrt.java
+++ inbltsrt.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 
 public class inbltsrt {
     public static void main(String args[]){
-        int arr[]= { 6,2,9,4,7};
-        Arrays.sort(arr);
+        int arr[]= { 5,3,2,4,1};
+       // Arrays.sort(arr);// output 1,2,3,4,5
+    Arrays.sort(arr,0,4);
         System.out.println(Arrays.toString(arr));
 }
 }
